d784cde80684363573d07c89eac0c9f6670cdd67
MarcoDaz <80964100+MarcoDaz@users.noreply.github.com>
2022-09-06 11:33:32 +0100

    setup workflow md

--- design/request_list.md
+++ design/request_list.md
@@ -6,4 +6,3 @@ GET /posts?id=1
 # Expected response:
 
 Response for 200 OK
-```
--- lib/seeds.sql
+++ lib/seeds.sql
@@ -1,11 +1,27 @@
-DROP TABLE IF EXISTS "public"."books";
+DROP TABLE IF EXISTS "public"."users",
+                     "public"."spaces",
+                     "public"."requests";
 -- This script only contains the table creation statements and does not fully represent the table in the database. It's still missing: indices, triggers. Do not use it as a backup.
 
 -- Sequence and defined type
-CREATE SEQUENCE IF NOT EXISTS books_id_seq;
+CREATE SEQUENCE IF NOT EXISTS users_id_seq, spaces_id_seq, requests_id_seq;
 
 -- Table Definition
-CREATE TABLE "public"."books" (
+CREATE TABLE "public"."users" (
+    "id" SERIAL,
+    "title" text,
+    "author_name" text,
+    PRIMARY KEY ("id")
+);
+
+CREATE TABLE "public"."spaces" (
+    "id" SERIAL,
+    "title" text,
+    "author_name" text,
+    PRIMARY KEY ("id")
+);
+
+CREATE TABLE "public"."requests" (
     "id" SERIAL,
     "title" text,
     "author_name" text,

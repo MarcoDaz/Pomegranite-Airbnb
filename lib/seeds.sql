90705f3d9144eea796e53135126ed2632978e16c
MarcoDaz <80964100+MarcoDaz@users.noreply.github.com>
2022-09-06 11:47:03 +0100

    setup seeds.sql file - need to add values

--- lib/seeds.sql
+++ lib/seeds.sql
@@ -1,36 +1,62 @@
-DROP TABLE IF EXISTS "public"."users",
-                     "public"."spaces",
-                     "public"."requests";
+DROP TABLE IF EXISTS
+  "public"."users",
+  "public"."spaces",
+  "public"."requests";
 -- This script only contains the table creation statements and does not fully represent the table in the database. It's still missing: indices, triggers. Do not use it as a backup.
 
 -- Sequence and defined type
-CREATE SEQUENCE IF NOT EXISTS users_id_seq, spaces_id_seq, requests_id_seq;
+CREATE SEQUENCE IF NOT EXISTS
+  users_id_seq,
+  spaces_id_seq,
+  requests_id_seq;
 
 -- Table Definition
 CREATE TABLE "public"."users" (
     "id" SERIAL,
-    "title" text,
-    "author_name" text,
+    "email" text,
+    "password" text,
     PRIMARY KEY ("id")
 );
 
 CREATE TABLE "public"."spaces" (
     "id" SERIAL,
-    "title" text,
-    "author_name" text,
+    "name" text,
+    "description" text,
+    "price" number,
+    "available_from" date,
+    "available_to" date,
+    "user_id" int,
     PRIMARY KEY ("id")
 );
 
 CREATE TABLE "public"."requests" (
     "id" SERIAL,
-    "title" text,
-    "author_name" text,
+    "space_id" int,
+    "owner_user_id" int,
+    "requester_user_id" int,
+    "date" date,
+    "confirmed" boolean, 
     PRIMARY KEY ("id")
 );
 
-INSERT INTO "public"."books" ("id", "title", "author_name") VALUES
-(1, 'Nineteen Eighty-Four', 'George Orwell'),
-(2, 'Mrs Dalloway', 'Virginia Woolf'),
-(3, 'Emma', 'Jane Austen'),
-(4, 'Dracula', 'Bram Stoker'),
-(5, 'The Age of Innocence', 'Edith Wharton');
+INSERT INTO "public"."users" ("email", "password")
+VALUES ()
+
+INSERT INTO "public"."spaces" (
+  "name",
+  "description",
+  "price",
+  "available_from",
+  "available_to",
+  "user_id",
+)
+VALUES ()
+
+INSERT INTO "public"."requests" (
+  "space_id",
+  "owner_user_id",
+  "requester_user_id",
+  "date",
+  "confirmed", 
+)
+VALUES ()

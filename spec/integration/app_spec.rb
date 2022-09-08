0c80e0654c8db337fccd207352ac1e053a901d6c
chimaamu <chimaamu@proton.me>
2022-09-08 17:18:24 +0100

    updating repository

--- spec/integration/app_spec.rb
+++ spec/integration/app_spec.rb
@@ -54,9 +54,9 @@ describe Application do
     it " login form" do
       response = get('/sign_in')
       expect(response.status).to eq(200)
-      expect(response.body).to include("method='post'")
-      expect(response.body).to include("action='/sign_in'")
-      expect(response.body).to include("name='email'")
+      expect(response.body).to include('method="POST"')
+      expect(response.body).to include('action="/sign_in"')
+      expect(response.body).to include('name="email"')
     end
   end
 
@@ -93,71 +93,26 @@ describe Application do
       expect(body).to include '<h1>List a Space</h1>'
       expect(body).to include '<form action="/create_space" method="POST">'
       expect(body).to include '<input type="text" name="name" id="name" required>'
-      expect(body).to include '<input type="text" name="description" id="description" required>'
-      expect(body).to include '<input type="number" name="price" id="price" required>'
-      expect(body).to include '<input type="date" name="available_from" id="available_from" required>'
-      expect(body).to include '<input type="date" name="available_to" id="available_to" required>'
-    end
-  end
-
-  context 'POST /create_space' do
-    it 'redirects to sign_in if not signed in' do
-      response = post('create_space')
-      
-      # redirect to /sign_in
-      expect(response.status).to eq 302
-    end
-
-    it 'creates a space if signed in and redirects to /spaces' do
-      post('/sign_in', email: '123@gmail.com', password: '123456')
-
-      response = post(
-        '/create_space',
-        name: 'Dreamland Hotel',
-        description: '123 Imaginary St.',
-        price: 999,
-        available_from: '2023-01-01',
-        available_to: '2023-12-01'
-      )
-
-      # redirect to /spaces
-      expect(response.status).to eq 302
-
-      repo = SpaceRepository.new
-      latest_space = repo.all.last
-
-      expect(latest_space.name).to eq 'Dreamland Hotel'
-      expect(latest_space.description).to eq '123 Imaginary St.'
-      expect(latest_space.price).to eq 999
-      expect(latest_space.available_from).to eq '2023-01-01'
-      expect(latest_space.available_to).to eq '2023-12-01'
+      expect(body).to include '<input type="text" description="description" id="description" required>'
+      expect(body).to include '<input type="number" price="price" id="price" required>'
+      expect(body).to include '<input type="date" available_from="available_from" id="available_from" required>'
+      expect(body).to include '<input type="date" available_to="available_to" id="available_to" required>'
     end
   end
 
   context 'GET /spaces' do
     it 'returns the spaces form' do
       response = get('/spaces')
-      repo = SpaceRepository.new
       expect(response.status).to eq(200)
 
-    it 'deletes a request if booking is denied' do 
-      response = post('/requested/2',confirmation: false)
-      repo = RequestRepository.new
-      expect(response.status).to eq(302)
-      expect(response.body).to eq('')
-      expect(repo.all.length).to eq 1
+      expect(response.body).to include '<h1>Book a space</h1>'
     end
   end
 
-  context 'GET /sign_out' do
-    it 'changes the header for spaces.erb' do
-      post('/sign_in', email: '123@gmail.com', password: '123456')
-
-      response1 = get('/spaces')
-      expect(response1.status).to eq 200
-      expect(response1.body).to include 'Sign Out'
-
-      get('sign_out')
+  context 'GET /spaces/:id' do
+    it 'returns spaces/1' do
+      response = get('/spaces/1')
+      expect(response.status).to eq(200)
 
       expect(response.body).to include '<h1>308 Negra Arroyo Lane, Albuquerque</h1>'
       expect(response.body).to include '<h2>Quaint house with pool out back</h2>'

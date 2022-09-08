69fe49aa118aadfca80f8d4d3d29d39b38b50359
MarcoDaz <80964100+MarcoDaz@users.noreply.github.com>
2022-09-08 09:21:20 +0100

    recovered /sign in code from before last pull request

--- app.rb
+++ app.rb
@@ -29,14 +29,34 @@ class Application < Sinatra::Base
     
     return erb(:sign_up_success)
   end
-  
-  get '/create_space' do
-    return erb(:create_space)
+
+  get '/sign_in' do
+    return erb(:sign_in)
   end
+
+  post '/sign_in' do
+    email = params[:email]
+    password = params[:password]
+    repo = UserRepository.new
+    user = repo.find_by_email(email)
+    redirect('/sign_in') unless repo.sign_in(email, password)
+    session[:id] = user.id
+    redirect('/spaces')
+  end
+
+  # get "/sign_out" do
+  #   session.clear
+  #   redirect("/")
+  # end 
 
   get '/spaces' do
     repo = SpaceRepository.new
     @spaces = repo.all
     return erb(:spaces)
   end
+
+  get '/create_space' do
+    return erb(:create_space)
+  end
+  
 end

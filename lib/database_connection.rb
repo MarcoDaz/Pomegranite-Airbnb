2de15e501676da68df67cc5ca26bd5c057cc7c1e
Connor <swagdecon77@gmail.com>
2022-09-09 12:38:14 +0100

    Changes database connection class to go to heroku

--- lib/database_connection.rb
+++ lib/database_connection.rb
@@ -8,10 +8,23 @@ class DatabaseConnection
   # This method connects to PostgreSQL using the 
   # PG gem. We connect to 127.0.0.1, and select
   # the database name given in argument.
-  def self.connect(database_name)
-    @connection = PG.connect({ host: '127.0.0.1', dbname: database_name })
+ # file: lib/database_connection.rb
+
+def self.connect
+  # If the environment variable (set by Heroku)
+  # is present, use this to open the connection.
+  if ENV['DATABASE_URL'] != nil
+    @connection = PG.connect(ENV['DATABASE_URL'])
+    return
   end
 
+  if ENV['ENV'] == 'test'
+    database_name = 'makersbnb_test'
+  else
+    database_name = 'makersbnb_test'
+  end
+  @connection = PG.connect({ host: '127.0.0.1', dbname: database_name })
+end
   # This method executes an SQL query 
   # on the database, providing some optional parameters
   # (you will learn a bit later about when to provide these parameters).

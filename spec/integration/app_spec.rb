d0142fffcde052abdb3c6bb815ffcd9e944f9cff
MarcoDaz <80964100+MarcoDaz@users.noreply.github.com>
2022-09-09 10:27:26 +0100

    fixed merge conflicts and setup testing for post /space/:id

--- spec/integration/app_spec.rb
+++ spec/integration/app_spec.rb
@@ -54,9 +54,9 @@ describe Application do
     it " login form" do
       response = get('/sign_in')
       expect(response.status).to eq(200)
-      expect(response.body).to include('method="POST"')
-      expect(response.body).to include('action="/sign_in"')
-      expect(response.body).to include('name="email"')
+      expect(response.body).to include("method='post'")
+      expect(response.body).to include("action='/sign_in'")
+      expect(response.body).to include("name='email'")
     end
   end
 
@@ -93,10 +93,44 @@ describe Application do
       expect(body).to include '<h1>List a Space</h1>'
       expect(body).to include '<form action="/create_space" method="POST">'
       expect(body).to include '<input type="text" name="name" id="name" required>'
-      expect(body).to include '<input type="text" description="description" id="description" required>'
-      expect(body).to include '<input type="number" price="price" id="price" required>'
-      expect(body).to include '<input type="date" available_from="available_from" id="available_from" required>'
-      expect(body).to include '<input type="date" available_to="available_to" id="available_to" required>'
+      expect(body).to include '<input type="text" name="description" id="description" required>'
+      expect(body).to include '<input type="number" name="price" id="price" required>'
+      expect(body).to include '<input type="date" name="available_from" id="available_from" required>'
+      expect(body).to include '<input type="date" name="available_to" id="available_to" required>'
+    end
+  end
+
+  context 'POST /create_space' do
+    it 'redirects to sign_in if not signed in' do
+      response = post('create_space')
+
+      # redirect to /sign_in
+      expect(response.status).to eq 302
+    end
+
+    it 'creates a space if signed in and redirects to /spaces' do
+      post('/sign_in', email: '123@gmail.com', password: '123456')
+
+      response = post(
+        '/create_space',
+        name: 'Dreamland Hotel',
+        description: '123 Imaginary St.',
+        price: 999,
+        available_from: '2023-01-01',
+        available_to: '2023-12-01'
+      )
+
+      # redirect to /spaces
+      expect(response.status).to eq 302
+
+      repo = SpaceRepository.new
+      latest_space = repo.all.last
+
+      expect(latest_space.name).to eq 'Dreamland Hotel'
+      expect(latest_space.description).to eq '123 Imaginary St.'
+      expect(latest_space.price).to eq 999
+      expect(latest_space.available_from).to eq '2023-01-01'
+      expect(latest_space.available_to).to eq '2023-12-01'
     end
   end
 
@@ -115,7 +149,6 @@ describe Application do
     it 'returns the spaces form' do
       response = get('/spaces')
       expect(response.status).to eq(200)
-
       expect(response.body).to include '<h1>Book a space</h1>'
     end
   end
@@ -125,6 +158,33 @@ describe Application do
       response = get('/spaces/1')
       expect(response.status).to eq(200)
 
+      expect(response.body).to include '<h1>308 Negra Arroyo Lane, Albuquerque</h1>'
+      expect(response.body).to include '<h2>Quaint house with pool out back</h2>'
+    end
+  end
+
+  context 'POST /spaces/:id' do
+    it 'redirects if not signed in ' do
+      response = post('/spaces/1')
+      expect(response.status).to eq 302
+    end
+
+    it 'creates a new request for the matching space' do
+      post('/sign_in', email: '123@gmail.com', password: '123456')
+
+      response = post(
+        '/spaces/1',
+        date: '2022-09-25'
+      )
+
+      latest_request = RequestRepository.new.all.last
+      expect(latest_request.space_id).to eq 1
+      expect(latest_request.owner_user_id).to eq 2
+      expect(latest_request.requester_user_id).to eq 1
+      expect(latest_request.date).to eq '2022-09-25'
+      expect(latest_request.confirmed).to eq false
+
+      expect(response.status).to eq 302
     end
   end
 
@@ -161,8 +221,15 @@ describe Application do
     it 'changes the header for spaces.erb' do
       post('/sign_in', email: '123@gmail.com', password: '123456')
 
-      expect(response.body).to include '<h1>308 Negra Arroyo Lane, Albuquerque</h1>'
-      expect(response.body).to include '<h2>Quaint house with pool out back</h2>'
+      response1 = get('/spaces')
+      expect(response1.status).to eq 200
+      expect(response1.body).to include 'Sign Out'
+
+      get('sign_out')
+
+      response2 = get('spaces')
+      expect(response2.status).to eq 200
+      expect(response2.body).to include 'Login'
     end
   end
 end 
